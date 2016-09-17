562c75d93bf43f70a89ea1f52d26d0f5118d6e9b
Aistis <smikislt@yahoo.com>
2016-09-17 18:46:00 +0100

    Initial commit

---
+++ Star Reader/Star Reader/FileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Star_Reader
+{
+    class FileReader
+    {
+        public void ProcessFile()
+        {
+        }
+    }
+}
---
+++ Star Reader/Star Reader/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Star_Reader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void ReadFile(object sender, RoutedEventArgs e)
+        {
+            FileReader reader = new FileReader();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                FileContentBox.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+    }
+}
