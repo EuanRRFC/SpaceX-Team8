7a92dfe5f30863a3b79379b6262ee369f421b76d
Deathscythe84 <alanchuntington@hotmail.com>
2016-09-19 10:55:54 +0100

    Initial Push of basic program, that i buggered to try and add that detailed view thing, still working on that.

---
+++ Star Reader/Star Reader/Model/FileReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Star_Reader.Model
+{
+    class FileReader
+    {
+
+        public FileReader()
+        {
+
+        }
+
+        public void storeRecording(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@path);
+            Recording r = new Recording();
+            r.addStartTime(Convert.ToDateTime(lines[0]));
+            r.addChannel(Int32.Parse(lines[1]));
+            r.addEndTime(Convert.ToDateTime(lines[lines.Length - 1]));
+
+            int jump = 5;
+            for (int i = 3; i < lines.Length - 3; i += jump)
+            {
+                DateTime dt = Convert.ToDateTime(lines[i]);
+                char pt = Convert.ToChar(lines[i + 1]);
+                Packet p;
+                if (pt == 'P')
+                {
+                    string payload = lines[i + 2];
+                    string message = lines[i + 3];
+                    string packetend = lines[i + 3];
+                    p = new Packet(dt, payload, pt, packetend);
+                    r.addPacket(p);
+                    jump = 5;
+                }
+                else if (pt == 'E')
+                {
+                    string ErrorType = lines[i + 2];
+                    p = new Packet(dt, pt, ErrorType);
+                    r.addPacket(p);
+                    jump = 4;
+                }
+
+            }
+            App.RecordStore[r.getChannel()] = r;
+        }
+    }
+}
---
+++ Star Reader/Star Reader/Model/Packet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Reader.Model
+{
+    class Packet
+    {
+        private DateTime time;
+        private string payload;
+        private char packetType;
+        private string errorType;
+        private string packetEnd;
+
+        public Packet(DateTime dt, string contents, char pt, string pe)
+        {
+            time = dt;
+            payload = contents;
+            packetType = pt;
+            packetEnd = pe;
+        }
+        public Packet(DateTime dt, char pt, string et)
+        {
+            time = dt;
+            packetType = pt;
+            errorType = et;
+        }
+
+        public char getPacketType()
+        {
+            return packetType;
+        }
+        public DateTime getPacketTime()
+        {
+            return time;
+        }
+        public string getPacketData()
+        {
+            return payload;
+        }
+        public string getErrorType()
+        {
+            return errorType;
+        }
+        public string getPacketEnd()
+        {
+            return packetEnd;
+        }
+
+    }
+}
