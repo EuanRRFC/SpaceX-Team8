0e690c29647b3b810f8499d418aaa4f07da03ab9
RossMarshall <ross.a.a.marshall@googlemail.com>
2016-09-22 15:11:24 +0100

    fixed namespace error

--- Star Reader/Star Reader/DetailsTab.xaml.cs
+++ Star Reader/Star Reader/DetailsTab.xaml.cs
@@ -21,6 +21,7 @@ namespace Star_Reader
 
         private ICollectionView dataGridCollection;
         private string filterString;
+        public string[] Labels { get; set; }
 
         public ICollectionView DataGridCollection
         {
@@ -145,7 +146,7 @@ namespace Star_Reader
                     switch (p.ErrorType)
                     {
                         case "Disconnect":
-                            btn1.Background = Brushes.Red;
+                            btn1.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#ff3333"));
                             btn1.ToolTip = p.Time + "." + p.Time.ToString("fff") + "\n" + p.PacketType + "\n" + p.ErrorType;
                             btn1.Content = p.ErrorType[0];
                             break;
@@ -166,8 +167,8 @@ namespace Star_Reader
                     else
                         if (p.PacketEnd.Equals("EEP"))
                         {
-                            btn1.Background = Brushes.Red;
-                            btn1.ToolTip = p.Time + "." + p.Time.ToString("fff") + "\n" + p.PacketType + "\n" + p.Payload + "\n" + p.PacketEnd;
+                            btn1.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#ff3333"));
+                        btn1.ToolTip = p.Time + "." + p.Time.ToString("fff") + "\n" + p.PacketType + "\n" + p.Payload + "\n" + p.PacketEnd;
                             btn1.Content = p.PacketEnd[0];
                         }
                         else
@@ -204,14 +205,15 @@ namespace Star_Reader
                 },
                 new RowSeries
                 {
-                    Title = "Disconnect",
+                    Title = "Errors",
                     Values = new ChartValues<double> {},
-                    DataLabels = true,
                     LabelPoint = point => point.X + ""
-                },
-                new RowSeries
+                }
+            };
+
+            Labels = new[]
                 {
-                    Title = "Parity",
+                    Title = "Pariyt",
                     Values = new ChartValues<double> {},
                     DataLabels = true,
                     LabelPoint = point => point.X + ""
@@ -257,11 +259,11 @@ namespace Star_Reader
         {
             Graphing getBars = new Graphing();
             List<double> bars = getBars.getBars(gData);
-                SeriesCollection[1].Values.Add(bars[0]);
-            SeriesCollection[2].Values.Add(bars[1]);
-            SeriesCollection[3].Values.Add(bars[2]);
-            SeriesCollection[4].Values.Add(bars[3]);
-            DataContext = this;
+            for (int x = 0; x <bars.Count; x++)
+            {
+                SeriesCollection[1].Values.Add(bars[x]);
+                DataContext = this;
+            }
         }
 
         private void radioButton2_Unchecked(object sender, RoutedEventArgs e)
@@ -270,7 +272,5 @@ namespace Star_Reader
         }
 
         public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> Formatter { get; set; }
     }
 }
